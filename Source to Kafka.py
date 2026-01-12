d98c75702582fa807cad46b7927b33bd98f9729e
jshivani123 <jainshivani172@gmail.com>
2026-01-12 12:59:50 +0530

    Update Source to Kafka.py
    Temporarily storing data in Kafka once received from source. Here, we are considering source as a CSV file, while in realtime it could be a API generating data continuously

--- Source to Kafka.py
+++ Source to Kafka.py
@@ -1,1 +1,16 @@
 
+from kafka import KafkaProducer
+import json
+from time import sleep
+
+producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
+
+# reading file
+file_data = open("Course4Dataset-data.csv","r")
+for line in file_data:
+  words = line.replace("\n","").split(",")
+  data = json.dumps({"id":words[0], "lat":words[1], "long":words[2], "ts":words[                                                                             3], "type":words[4]}).encode('utf-8')
+  ack = producer.send("surgePriceDemo",value = data)
+  metadata = ack.get()
+  print(metadata.topic,metadata.partition)
+  sleep(1)
